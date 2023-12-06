fa33916fe098a5c56c2d2f646b7e6ef9e5e7ffe1
Ankit <ankit.raut@coditas.com>
2023-12-06 17:30:34 +0530

    Deployment changes made

--- Execution-Script/DockerScript.sh
+++ Execution-Script/DockerScript.sh
@@ -7,34 +7,17 @@
 setVariables()
 {
     #defining directory path.
-    # path="/home/ubuntu/Node-Project"
-    path="/home/ankitraut0987/shopping-project"
-    
-    #defining nginx path
-    nginx_path="/etc/nginx/sites-enabled"
-
+    path="/home/ankitraut0987/StaticIP-MultiVM-DockerProject"
 }
 
 getVariables()
 {
     echo "Path:$path"
-    echo "Nginx Path:$nginx_path"
 
-}
+}W
 
 
 setVariables
-
-#Install Nginx Server
-sudo apt-get install nginx -y >/dev/null || { echo "Failed to Install Nginx Server"; exit 1; }
-
-#Configuring Nginx Server:
-sudo rm "$nginx_path/default" || echo "Default Config File Not Found"
-cd "$path/Execution-Script/"
-sudo cp "default" "$nginx_path/"
-sudo systemctl restart nginx
-
-echo "*** Successfully Configured Nginx ***"
 
 cd
 
@@ -46,26 +29,4 @@ cd "$path"
 
 sudo docker-compose down && echo "Docker Is Down Now" || echo "Docker Already Down"
 
-#sudo docker rm shopping-project_frontend_1
-#sudo docker rm shopping-project_backend_1
-
-sudo docker rmi shopping-project_frontend || echo "Error deleting frontend image"
-sudo docker rmi shopping-project_backend || echo "Error deleting backend image"
-
 sudo docker-compose up -d || echo "error in compose file"
-
-
-
-#-----------------------------------------------------------------------------------------------------------------------------------------
-# backend_container_id=$(sudo docker ps -a | grep backend | awk '{print $1}')
-# sudo docker rm $backend_container_id 2>/dev/null || echo "Backend Container Not Found"
-
-# frontend_container_id=$(sudo docker ps -a | grep frontend | awk '{print $1}')
-# sudo docker rm $frontend_container_id 2>/dev/null || echo "Frontend Container Not Found"
-
-# backend_image_id=$(sudo docker images | grep backend | awk '{print $3}')
-# sudo docker rmi $backend_image_id  2>/dev/null || echo "Backend Image Not Found"
-
-# frontend_image_id=$(sudo docker images | grep frontend | awk '{print $3}')
-# sudo docker rmi $frontend_image_id 2>/dev/null || echo "Frontend Image Not Found"
-#-----------------------------------------------------------------------------------------------------------------------------------------
