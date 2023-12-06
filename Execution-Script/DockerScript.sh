16ed5272baab932eb973e51399f0cf6abdc7e6aa
Ankit <ankit.raut@coditas.com>
2023-12-06 17:40:23 +0530

    deployment changes made in backend

--- Execution-Script/DockerScript.sh
+++ Execution-Script/DockerScript.sh
@@ -7,8 +7,7 @@
 setVariables()
 {
     #defining directory path.
-    # path="/home/ubuntu/Node-Project"
-    path="/home/ankitraut0987/shopping-project"
+    path="/home/ankitraut0987/StaticIP-MultiVM-DockerProject"
     
     #defining nginx path
     nginx_path="/etc/nginx/sites-enabled"
@@ -46,26 +45,7 @@ cd "$path"
 
 sudo docker-compose down && echo "Docker Is Down Now" || echo "Docker Already Down"
 
-#sudo docker rm shopping-project_frontend_1
-#sudo docker rm shopping-project_backend_1
-
-sudo docker rmi shopping-project_frontend || echo "Error deleting frontend image"
-sudo docker rmi shopping-project_backend || echo "Error deleting backend image"
+sudo docker rmi StaticIP-MultiVM-DockerProject_backend || echo "Error deleting backend image"
 
 sudo docker-compose up -d || echo "error in compose file"
 
-
-
-#-----------------------------------------------------------------------------------------------------------------------------------------
-# backend_container_id=$(sudo docker ps -a | grep backend | awk '{print $1}')
-# sudo docker rm $backend_container_id 2>/dev/null || echo "Backend Container Not Found"
-
-# frontend_container_id=$(sudo docker ps -a | grep frontend | awk '{print $1}')
-# sudo docker rm $frontend_container_id 2>/dev/null || echo "Frontend Container Not Found"
-
-# backend_image_id=$(sudo docker images | grep backend | awk '{print $3}')
-# sudo docker rmi $backend_image_id  2>/dev/null || echo "Backend Image Not Found"
-
-# frontend_image_id=$(sudo docker images | grep frontend | awk '{print $3}')
-# sudo docker rmi $frontend_image_id 2>/dev/null || echo "Frontend Image Not Found"
-#-----------------------------------------------------------------------------------------------------------------------------------------
