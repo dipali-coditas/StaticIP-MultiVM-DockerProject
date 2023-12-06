f6155955118f87ec964c70e64e5b35ab21db8d1f
Ankit <ankit.raut@coditas.com>
2023-12-06 17:49:30 +0530

    backend final deployment

--- Execution-Script/DockerScript.sh
+++ Execution-Script/DockerScript.sh
@@ -8,32 +8,17 @@ setVariables()
 {
     #defining directory path.
     path="/home/ankitraut0987/StaticIP-MultiVM-DockerProject"
-    
-    #defining nginx path
-    nginx_path="/etc/nginx/sites-enabled"
 
 }
 
 getVariables()
 {
     echo "Path:$path"
-    echo "Nginx Path:$nginx_path"
 
 }
 
 
 setVariables
-
-#Install Nginx Server
-sudo apt-get install nginx -y >/dev/null || { echo "Failed to Install Nginx Server"; exit 1; }
-
-#Configuring Nginx Server:
-sudo rm "$nginx_path/default" || echo "Default Config File Not Found"
-cd "$path/Execution-Script/"
-sudo cp "default" "$nginx_path/"
-sudo systemctl restart nginx
-
-echo "*** Successfully Configured Nginx ***"
 
 cd
 
