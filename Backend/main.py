41e1f6e6e343b47fd70778c1b69b90ffb91b0be3
Dipali Dhanwade <dipali.dhanwade@coditas.com>
2023-12-26 12:40:46 +0530

    Added postgres to replace mongodb

--- Backend/main.py
+++ Backend/main.py
@@ -1,30 +1,65 @@
+# from flask import Flask, jsonify
+# from flask_cors import CORS
+# from pymongo import MongoClient
+
+# app = Flask(__name__)
+# CORS(app)
+
+# # for manual container deployement tyr this 
+# # client = MongoClient("mongodb://localhost:27017")
+
+# client = MongoClient("mongodb://root:root@192.0.0.5:27017")
+
+
+# db = client["shopping"]
+# collection = db["products"]
+
+# @app.route('/get_data', methods=['GET'])
+# def get_data():
+#     # Fetch data from MongoDB
+#     cursor = collection.find({})
+#     data = list(cursor)
+
+#     # Convert ObjectId to str for JSON serialization
+#     for item in data:
+#         item['_id'] = str(item['_id'])
+#     print(data)
+#     return jsonify(data)
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", debug=True, port=8000)
+
 from flask import Flask, jsonify
 from flask_cors import CORS
-from pymongo import MongoClient
+import psycopg2
 
 app = Flask(__name__)
 CORS(app)
 
-# for manual container deployement tyr this 
-# client = MongoClient("mongodb://localhost:27017")
+# Connect to PostgreSQL
+conn = psycopg2.connect(
+    dbname="postgres",
+    user="postgres",
+    password="root",
+    host="localhost",
+    port="5432"
+)
 
-client = MongoClient("mongodb://root:root@192.0.0.5:27017")
-
-
-db = client["shopping"]
-collection = db["products"]
+# Create a cursor
+cursor = conn.cursor()
 
 @app.route('/get_data', methods=['GET'])
 def get_data():
-    # Fetch data from MongoDB
-    cursor = collection.find({})
-    data = list(cursor)
+    # Fetch data from PostgreSQL
+    cursor.execute("SELECT * FROM products")
+    data = cursor.fetchall()
 
-    # Convert ObjectId to str for JSON serialization
-    for item in data:
-        item['_id'] = str(item['_id'])
-    print(data)
-    return jsonify(data)
+    # Convert data to a list of dictionaries
+    keys = ["pid", "name", "price", "description", "image_url"]
+    data_list = [dict(zip(keys, row)) for row in data]
+
+    return jsonify(data_list)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=8000)
+
