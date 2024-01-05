50150b29d8f137c733ed7785c7280645d41942cf
dipali-coditas <130266261+dipali-coditas@users.noreply.github.com>
2024-01-05 10:23:30 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,67 +1,30 @@
-# from flask import Flask, jsonify
-# from flask_cors import CORS
-# from pymongo import MongoClient
-
-# app = Flask(__name__)
-# CORS(app)
-
-# # for manual container deployement tyr this 
-# # client = MongoClient("mongodb://localhost:27017")
-
-# client = MongoClient("mongodb://root:root@192.0.0.5:27017")
-
-
-# db = client["shopping"]
-# collection = db["products"]
-
-# @app.route('/get_data', methods=['GET'])
-# def get_data():
-#     # Fetch data from MongoDB
-#     cursor = collection.find({})
-#     data = list(cursor)
-
-#     # Convert ObjectId to str for JSON serialization
-#     for item in data:
-#         item['_id'] = str(item['_id'])
-#     print(data)
-#     return jsonify(data)
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", debug=True, port=8000)
-
 from flask import Flask, jsonify
 from flask_cors import CORS
-import psycopg2
+from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)
 
-# Connect to PostgreSQL
-conn = psycopg2.connect(
-    dbname="postgres",
-    user="postgres",
-    password="root",
-    host="34.93.85.33",
-    port="5432"
-)
+# Uncommented for manual container deployment, try this
+# client = MongoClient("mongodb://localhost:27017")
 
-# Create a cursor
-cursor = conn.cursor()
+# Use the following line for connecting to MongoDB in a container
+client = MongoClient("mongodb://root:root@35.244.15.94:27017")
+
+db = client["shopping"]
+collection = db["products"]
 
 @app.route('/get_data', methods=['GET'])
 def get_data():
-    # Fetch data from PostgreSQL
-    cursor.execute("SELECT * FROM products")
-    data = cursor.fetchall()
+    # Fetch data from MongoDB
+    cursor = collection.find({})
+    data = list(cursor)
 
-    # Convert data to a list of dictionaries
-    keys = ["pid", "name", "price", "description", "image_url"]
-    data_list = [dict(zip(keys, row)) for row in data]
-    # conn.close()
-
-    return jsonify(data_list)
-
+    # Convert ObjectId to str for JSON serialization
+    for item in data:
+        item['_id'] = str(item['_id'])
+    print(data)
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=8000)
-
